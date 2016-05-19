d440a1d474e00f0bac10198e456c1a7199f99561
Monika Schrenk <moni@random-access.org>
2016-05-19 12:26:27 +0200

    Deleted unnecessary folder

--- PatchedWebsite/VulnerableWebsite/public/template/footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Footer -->
-    <div class="navbar navbar-inverse navbar-fixed-bottom">
-        <div class="container">
-          <p class="navbar-text pull-right">© 2016 - Monika Schrenk</p>
-        </div>
-    </div>
