b325d6107ae2ad2a2f9ba48814222ff5449d2f05
Monika Schrenk <moni@random-access.org>
2016-05-28 12:35:21 +0200

    Fixed bug in logout of dev-fixed

--- src-vuln/public/authenticate.php
+++ src-vuln/public/authenticate.php
@@ -4,7 +4,7 @@
     session_start();
 
     // redirect to login page if no authenticated user
-    if (!isset($_SESSION["status"]) || !$_SESSION["status"] == "loggedin") {
+    if (!(isset($_SESSION["status"]) && $_SESSION["status"] == "loggedin")) {
         header("Location: index.php");
         exit;
     }
